cab977282ca84eb8f8e0e248205bff36baf6bda3
decepticlown <50569653+decepticlown@users.noreply.github.com>
2021-03-27 13:34:54 +0530

    Update lock.sh

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -4,18 +4,18 @@ i3lock \
 	-B 2 \
 	--force-clock \
 	--indicator \
-	--insidevercolor=#2e2e2fff \
-	--insidewrongcolor=#2e2e2fff \
-	--insidecolor=#2e2e2fff \
-	--ringvercolor=#7cbbe6ff \
-	--ringwrongcolor=#be3badff \
-	--ringcolor=#2e2e2fff \
+	--insidevercolor=#2e2e2faa \
+	--insidewrongcolor=#2e2e2faa \
+	--insidecolor=#2e2e2faa \
+	--ringvercolor=#7cbbe6aa \
+	--ringwrongcolor=#be3badaa \
+	--ringcolor=#2e2e2f00 \
 	--line-uses-inside \
-	--keyhlcolor=#7cbbe6ff \
-	--bshlcolor=#be3badff \
-	--separatorcolor=#2e2e2fff \
-	--verifcolor=#7cbbe6ff \
-	--wrongcolor=#be3badff \
+	--keyhlcolor=#7cbbe6aa \
+	--bshlcolor=#be3badaa \
+	--separatorcolor=#2e2e2faa \
+	--verifcolor=#7cbbe6aa \
+	--wrongcolor=#be3badaa \
 	--indpos="960:540" \
 	--timecolor=#7cbbe6ff \
 	--timestr="%H:%M" \
@@ -23,7 +23,7 @@ i3lock \
 	--datecolor=#7f7f7fff \
 	--datestr="%A, %m %Y" \
 	--timesize=36 \
-	--datesize=22 \
+	--datesize=20 \
 	--greetersiz=22 \
 	--verifsize=22 \
 	--wrongsize=22 \
@@ -40,4 +40,7 @@ i3lock \
 	--ring-width 5.0
 
 # Turn the screen off after a delay.
-sleep 10; pgrep i3lock && xset dpms force off
+sleep 15; pgrep i3lock && xset dpms force off
+
+# Hibernate after 15min
+sleep 300; pgrep i3lock && systemctl hibernate
