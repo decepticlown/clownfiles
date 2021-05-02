152a776eabfea09ff0693cf2cacbd70394fbc352
a3p169 <50569653+a3p169@users.noreply.github.com>
2021-05-02 22:42:01 +0530

    Delete i3 directory

--- i3/volume_minus.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ $(($(awk -F" /" '/front-left:/ { print $2 }' <(pactl list sinks) | tr -dc '0-9') + 0)) -gt 0 ]]; then
-	pactl set-sink-volume 0 -1% && 
-		killall -SIGUSR1 i3status && 
-		dunstify -r 10022 -a Volume $(awk -F" /" '/front-left:/ { print $2 }' <(pactl list sinks)) -u low
-fi
